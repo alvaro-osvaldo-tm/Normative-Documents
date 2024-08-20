844604ccefd9c392c76dea129ec8b5f9bc282c10
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2024-08-20 16:54:09 -0300

    build (sys): Added 'shellcheck' for static code analysis

---
+++ Tools/development/lint/lint-all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# Lint or do static code check on all source code
+#
+
+cd "$(dirname "$0")/../../.." || exit 1
+
+
+# Lint with prettier
+npx prettier --write .
+
+
+# Lint all shell files
+find . -type f -name '*.sh' ! -wholename '*/.npm/*' ! -wholename "*/node_modules/*" ! -wholename "./.husky/*" -exec npx shellcheck '{}' \;
+
+# Lint all husky git hooks
+ find .husky -type f  -maxdepth 1 -exec npx shellcheck -e SC1081 '{}' \;
