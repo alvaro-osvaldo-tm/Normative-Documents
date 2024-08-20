426a5fd8d077d25114ddd7e260e66dd133d9f4d6
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2024-08-20 16:55:56 -0300

    build: Added 'lint' as type for commit messages specification

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,142 +1,83 @@
-import type { UserConfig } from '@commitlint/types';
-import { RuleConfigSeverity } from '@commitlint/types';
-
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
 
 /**
  * Return all valid scopes
  * @returns A list of scope altennatives
  */
 function scopes(): string[] {
+	/**
+	 * Get all directories in 'taxonomy' and return their
+	 * names formated as scope.
+	 */
 
-    /**
-     * Get all directories in 'taxonomy' and return their
-     * names formated as scope.
-     */
+	const fs = require("node:fs");
 
-    const fs = require('node:fs');
+	const folderPath = "./Taxonomy";
 
-    const folderPath = './Taxonomy';
+	const scopes = fs
+		.readdirSync(folderPath)
+		.map((item: string) => {
+			return item.toLowerCase();
+		})
+		.filter((item: string) => {
+			/**
+			 * Ignore:
+			 *  - hidden files, like '.gitignore'
+			 *  - Markdown documents, like 'README.md'
+			 */
+			if (item.startsWith(".")) return false;
 
-    const scopes = fs.readdirSync(folderPath)
-        .map((item: string) => {
-            return item.toLowerCase()
-        })
-        .filter((item: string) => {
+			if (item.endsWith(".md")) return false;
 
-            /**
-             * Ignore: 
-             *  - hidden files, like '.gitignore'
-             *  - Markdown documents, like 'README.md'
-             */
-            if (item.startsWith('.'))
-                return false;
+			return true;
+		});
 
-            if (item.endsWith('.md'))
-                return false;
-
-            return true;
-        });
-
-
-    return scopes;
-
-
-
+	return scopes;
 }
 
-
 const Configuration: UserConfig = {
+	extends: ["@commitlint/config-conventional"],
+	parserPreset: "conventional-changelog-atom",
+	formatter: "@commitlint/format",
+	rules: {
+		"header-max-length": [RuleConfigSeverity.Warning, "always", 72],
+		"header-trim": [RuleConfigSeverity.Error, "always"],
+		"header-full-stop": [RuleConfigSeverity.Error, "never", "."],
+		"signed-off-by": [
+			RuleConfigSeverity.Warning,
+			"always",
+			"Signed-off-by:",
+		],
+		"body-full-stop": [RuleConfigSeverity.Error, "never", "."],
+		"subject-empty": [RuleConfigSeverity.Warning, "always"],
+		"subject-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
+		"scope-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
 
-    extends: ['@commitlint/config-conventional'],
-    parserPreset: 'conventional-changelog-atom',
-    formatter: '@commitlint/format',
-    rules: {
+		"scope-enum": [RuleConfigSeverity.Error, "always", scopes()],
 
-        'header-max-length': [
-            RuleConfigSeverity.Warning,
-            'always',
-            72
-        ],
-        'header-trim': [
-            RuleConfigSeverity.Error,
-            'always'
-        ],
-        'header-full-stop': [
-            RuleConfigSeverity.Error,
-            'never',
-            '.'
-        ],
-        'signed-off-by': [
-            RuleConfigSeverity.Warning,
-            'always',
-            'Signed-off-by:'
-        ],
-        'body-full-stop': [
-            RuleConfigSeverity.Error,
-            'never',
-            '.'
-        ],
-        'subject-empty': [
-            RuleConfigSeverity.Warning,
-            'always'
-        ],
-        'subject-case': [
-            RuleConfigSeverity.Error,
-            'always',
-            'sentence-case'
-        ],
-        'scope-case': [
-            RuleConfigSeverity.Error,
-            'always',
-            'sentence-case'
-        ],
+		"type-empty": [RuleConfigSeverity.Warning, "always"],
 
-        'scope-enum': [
-            RuleConfigSeverity.Error,
-            'always',
-            scopes()
-        ],
-
-        'type-empty': [
-            RuleConfigSeverity.Warning,
-            'always'
-        ],
-
-        'type-enum': [
-            RuleConfigSeverity.Error,
-            'always',
-            [
-                'build',
-                'rm',
-                'ci',
-                'spec',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'ui',
-                'test'
-            ]
-        ]
-
-    },
-
+		"type-enum": [
+			RuleConfigSeverity.Error,
+			"always",
+			[
+				"build",
+				"lint",
+				"rm",
+				"ci",
+				"spec",
+				"docs",
+				"feat",
+				"fix",
+				"perf",
+				"refactor",
+				"revert",
+				"ui",
+				"test",
+			],
+		],
+	},
 };
 
 export default Configuration;
-
-/*export const Configuration = {
-
-    extends: [
-        '@commitlint/config-conventional'
-    ],
-    parserPreset: 'conventional-changelog-atom',
-    formatter: '@commitlint/format',*/
-/*rules: {
-
-
-  
-}
-}*/
